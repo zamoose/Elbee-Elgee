efe362b4306f111430628d922db2370087726a1e
zamoose <none@none>
2006-10-03 14:22:26 +0000

    Changing the "hot dates" placement - it was just too funky in its previous incarnation - this should lead to more consistent placement.
    --HG--
    extra : convert_revision : svn%3Af5dc4d67-db16-0410-892f-c7aa27e68784/trunk%4059

--- theloop.php
+++ theloop.php
@@ -1,18 +1,18 @@
 	 <?php /* Start The Loop */ if (have_posts()) { while (have_posts()) { the_post(); ?>
 		<?php /* Permalink navigation has to be inside the loop */ if (is_single()) include (TEMPLATEPATH . '/navigation.php'); ?>
-		<?php 
-			$time = get_the_time('M d Y');
-			list($mo, $da, $ye) = explode(" ", $time);
-		?>
-		<acronym class="published" title="<?php the_time('Y-m-d\TG:i:sO'); ?>">
-			<span class="pub-month"><?php echo($mo); ?></span>
-			<span class="pub-date"><?php echo($da); ?></span>
-			<!--span class="pub-year"><?php echo($ye); ?></span-->
-		</acronym>	
 		<div id="post-<?php the_ID(); ?>" class="wppost">
 			<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php echo strip_tags(get_the_title()) ?>"><?php the_title(); ?></a></h3>	
 			<span class="postmeta">Posted by <?php the_author(); ?> | <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?><?php edit_post_link(' | Edit this entry.', '', ''); ?></span>
 			<div class="itemtext">
+				<?php 
+					$time = get_the_time('M d Y');
+					list($mo, $da, $ye) = explode(" ", $time);
+				?>
+				<acronym class="published" title="<?php the_time('Y-m-d\TG:i:sO'); ?>">
+					<span class="pub-month"><?php echo($mo); ?></span>
+					<span class="pub-date"><?php echo($da); ?></span>
+					<!--span class="pub-year"><?php echo($ye); ?></span-->
+				</acronym>
 					<?php if ( is_archive() or is_search() or (function_exists('is_tag') and is_tag()) ) {
 						the_excerpt();
 					} else {
