c0f387c361f3c159eb7a3440a2f5aade862901fd
zamoose <none@none>
2007-03-27 20:04:28 +0000

    Additions for making archives.php not look polly-wolly-crappy.
    Still more work to do, however.
    
    --HG--
    extra : convert_revision : svn%3Af5dc4d67-db16-0410-892f-c7aa27e68784/trunk%40103

--- archives.php
+++ archives.php
@@ -5,18 +5,15 @@ Template Name: Archives
 ?>
 <?php get_header(); ?>
 
-<div id="wrapper">
-	<div id="content">
-	<h2>Archives<br />By Date</h2>
-	<?php arl_subtraction_archives_posts(); ?>
+<div id="archive-wrapper">
+	<div id="archive-posts">
+		<h2>Archives<br />By Date</h2>
+		<?php arl_subtraction_archives_posts(); ?>
 	</div>
 </div>
-<div id="navigation">
+<div id="archive-categories">
 	<h2>Archives<br />By Category</h2>
 	<?php arl_subtraction_archives_categories(); ?>
-</div>
-<div id="extra">
-	&nbsp;
 </div>
 
 <?php get_footer(); ?>
