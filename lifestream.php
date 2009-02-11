bbc4391cd8b677c4fc6a4a40572ec81b53c13304
zamoose <none@none>
2009-02-11 04:57:19 +0000

    Adding the JS includes and the Lifestream page template to the repo.
    --HG--
    extra : convert_revision : svn%3Af5dc4d67-db16-0410-892f-c7aa27e68784/trunk%40187

---
+++ lifestream.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Lifestream
+*/
+?>
+<?php get_header(); ?>
+
+<div id="allwrapper">
+<div id="wrapper">
+	<div id="content">
+	<h3>Lifestream</h3>
+		<?php 
+			if(function_exists(simplelife)){
+				simplelife();
+			}
+
+			if(function_exists(lifestream)){
+				lifestream();
+			}
+		?>
+	</div>
+</div>
+
+<?php get_sidebar(); ?>
+
+</div>
+<?php get_footer(); ?>
