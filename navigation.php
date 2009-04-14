68fb3d3816d85aa25dc7dd06ea10629ca3058871
zamoose <none@none>
2009-04-14 21:32:52 +0000

    Moving all kinds of stuff out of the trunk -- none of this FLIR or fonts stuff is even used in the slightest.
    --HG--
    extra : convert_revision : svn%3Af5dc4d67-db16-0410-892f-c7aa27e68784/trunk%40195

--- navigation.php
+++ navigation.php
@@ -9,13 +9,15 @@
 	</div>
 
 	<?php } else { ?>
-		
 	<div class="navigation">
+			<?php if(function_exists('wp_pagenavi')) {
+ 					wp_pagenavi(); 
+				} else { ?>
 		<div class="left"><?php next_posts_link('<span>&laquo;</span> Older Entries') ?></div>
 		<div class="right"><?php previous_posts_link('Newer Entries <span>&raquo;</span>') ?></div>
 		<div class="clear"></div>
+			<?php } ?>
 	</div>
-
 	<?php } ?>
 
 	<!--hr /-->
