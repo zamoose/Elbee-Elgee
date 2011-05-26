795e2463db27edf018768570af00d2a99771d014
Doug Stewart <zamoose@gmail.com>
2011-05-26 10:09:18 -0400

    bp-navigation template fixes.

--- bp-navigation.php
+++ bp-navigation.php
@@ -1,29 +1,35 @@
-<ul id="lb-bp-nav">
+<div id="lb-bp-nav">
 
-<?php
-echo bp_get_loggedin_user_nav();
-//echo bp_get_userbar();
-//echo bp_get_optionsbar();
+	<div id="lb-bp-search-bar">
+		<div class="bp-padder">
 
-?>
-</ul><!-- #lb-bp-nav -->
+		<?php if ( bp_search_form_enabled() ) : ?>
 
-<div id="lb-bp-search-bar">
-	<div class="bp-padder">
+			<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
+				<input type="text" id="search-terms" name="search-terms" value="" />
+				<?php echo bp_search_form_type_select() ?>
 
-	<?php if ( bp_search_form_enabled() ) : ?>
+				<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
+				<?php wp_nonce_field( 'bp_search_form' ) ?>
+			</form><!-- #search-form -->
 
-		<form action="<?php echo bp_search_form_action() ?>" method="post" id="search-form">
-			<input type="text" id="search-terms" name="search-terms" value="" />
-			<?php echo bp_search_form_type_select() ?>
+		<?php endif; ?>
 
-			<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ) ?>" />
-			<?php wp_nonce_field( 'bp_search_form' ) ?>
-		</form><!-- #search-form -->
+		<?php do_action( 'bp_search_login_bar' ) ?>
 
-	<?php endif; ?>
+		</div><!-- .bp-padder -->
+	</div><!-- #lb-bp-search-bar -->
+	
+	<?php
+		wp_nav_menu( array( 'theme_location'	=> 'lblgbpmenu',  
+							'container'			=> 'ul',
+							'container_id'		=> 'lblgbpmenu',
+							'depth'				=> '1'
+				) );
+	//echo bp_get_loggedin_user_nav();
+	//echo bp_get_userbar();
+	//echo bp_get_optionsbar();
 
-	<?php do_action( 'bp_search_login_bar' ) ?>
-
-	</div><!-- .bp-padder -->
-</div><!-- #lb-bp-search-bar -->
+	?>
+	
+</div><!-- #lb-bp-nav -->
